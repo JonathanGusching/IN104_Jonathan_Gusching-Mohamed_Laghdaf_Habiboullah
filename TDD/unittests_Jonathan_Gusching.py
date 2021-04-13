c2ff320005ff39d550572d109a36df3b238974bc
Jonathan Gusching <81634945+JonathanGusching@users.noreply.github.com>
2021-04-13 17:24:39 +0200

    Update unittests_Jonathan_Gusching.py

--- TDD/unittests_Jonathan_Gusching.py
+++ TDD/unittests_Jonathan_Gusching.py
@@ -1,9 +1,9 @@
 import unittest
 import unittests_Mohamed_Laghdaf
+import unittests_Jonathan_Gusching
 
 def main():
 
 
 if __name__ == "__main__":
-    # execute only if run as a script
     main()
