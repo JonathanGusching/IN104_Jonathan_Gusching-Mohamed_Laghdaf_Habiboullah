66498a02bdc052dacbf3b04919efac08a454b36b
Jonathan Gusching <81634945+JonathanGusching@users.noreply.github.com>
2021-04-13 17:25:59 +0200

    Update unittests_Jonathan_Gusching.py

--- TDD/unittests_Jonathan_Gusching.py
+++ TDD/unittests_Jonathan_Gusching.py
@@ -1,9 +1,14 @@
 import unittest
 import unittests_Mohamed_Laghdaf
 import unittests_Jonathan_Gusching
+import Eleve
 
 def main():
-
+    eleve1=Eleve.eleve()
+    eleve1.test1()
+    eleve1.test2()
+    eleve1.test3()
+    eleve1.test4()
 
 if __name__ == "__main__":
     main()
