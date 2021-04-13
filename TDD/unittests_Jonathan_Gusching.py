7d159d419e22d52a2d921405489ae874c9bf756f
Jonathan Gusching <81634945+JonathanGusching@users.noreply.github.com>
2021-04-13 17:46:37 +0200

    Update unittests_Jonathan_Gusching.py

--- TDD/unittests_Jonathan_Gusching.py
+++ TDD/unittests_Jonathan_Gusching.py
@@ -4,11 +4,16 @@ import unittests_Jonathan_Gusching
 import Eleve
 
 def main():
-    eleve1=Eleve.eleve()
-    eleve1.test1()
-    eleve1.test2()
-    eleve1.test3()
-    eleve1.test4()
+    eleve1=Eleve.Eleve(20,264157,"Pierre Dupont",16.3)
+    eleve2=Eleve.Eleve(-1,264157,"Jeanne Dupont",16.3)
+    eleve3=Eleve.Eleve(18,2641575,"Jeanne d'Arc",10.9)
+    eleve4=Eleve.Eleve(18,26415,"Quelqu'un",22)
+    eleve1.testAge()
+    eleve1.testID()
+    eleve1.testMarks()
+    eleve2.testAge()
+    eleve3.testID()
+    eleve4.testMarks()
 
 if __name__ == "__main__":
     main()
