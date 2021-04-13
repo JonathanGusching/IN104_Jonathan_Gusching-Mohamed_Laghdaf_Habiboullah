39c8638beb5ebf238ad2b37efde446988ade9722
MohamedLaghdafHABIBOULLAH <81633807+MohamedLaghdafHABIBOULLAH@users.noreply.github.com>
2021-04-13 17:27:22 +0200

    Update Eleve.py

--- TDD/Eleve.py
+++ TDD/Eleve.py
@@ -6,3 +6,7 @@ class Eleve(unittest.TestCase) :
         self.age = age
     def __str__(self) :
         return f'L'Eleve s'appelle {self.name}, d'age {self.age} et son ID est {self.ID}' 
+    def getID(self) :
+    	return (self.ID)
+    def update_ID(self,ID2) :
+        self.ID=ID2
