3968e06afc2ca304369599b55b8a99a5e8bd5b1c
MohamedLaghdafHABIBOULLAH <81633807+MohamedLaghdafHABIBOULLAH@users.noreply.github.com>
2021-04-20 16:01:19 +0200

    Update unittests_Mohamed_Laghdaf.py

--- TDD/unittests_Mohamed_Laghdaf.py
+++ TDD/unittests_Mohamed_Laghdaf.py
@@ -1,1 +1,14 @@
 import unittest
+import unittests_Jonathan_Gusching
+import Batiment
+
+def main():
+    Bat1=Batiment.Batiment(460,"A","Palaisaeu","ResidenceEnsta")
+    Bat2=Batiment.Batiment(900,"T","Palaiseau","ResidenceEnsta")
+    Bat3=Batiment.Batiment(480,"S","Palaiseau","ResidenceEnsta")
+    Bat4=Batiment.Batiment(560,"N","Palaiseau","ResidenceEnsta")
+    Bat1.testprice()
+    Bat2.testprice()
+
+if __name__ == "__main__":
+    main()
