2135821496c13b5df0f20bdf4cf244e2252096fb
MohamedLaghdafHABIBOULLAH <81633807+MohamedLaghdafHABIBOULLAH@users.noreply.github.com>
2021-05-09 14:58:06 +0200

    Update cnn.py
    Import not used

--- cnn.py
+++ cnn.py
@@ -11,13 +11,11 @@ from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
 
-from sklearn.metrics import classification_report,confusion_matrix
 
 import tensorflow as tf
 
 import cv2
 import os
-import joblib
 import numpy as np
 
 
