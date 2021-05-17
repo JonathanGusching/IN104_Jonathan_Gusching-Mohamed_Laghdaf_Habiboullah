9aa87efd83e0f3d5eab273aefe2b27525f8b6de9
JonathanGusching <jonathan.gusching@ensta-paris.fr>
2021-05-18 00:22:34 +0200

    It should be working but there are memory issues (especially in predictor). Must solve it tomorrow

--- text.py
+++ text.py
@@ -1,16 +1,28 @@
 import pandas as pd
+import numpy as np
+from lime import lime_text
+import torch
+import torch.nn.functional as F
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+#uses PyTorch
+
+device="cuda"
+torch.device(device)
 
 #We can't use our initial dataset (1GB), too heavy, so instead... :
 file_name="Reviews.csv"
 
 
 ds = pd.read_csv(file_name)
-ds=ds.iloc[56,9] #Getting the right column, i.e. the one with the review
+ds=ds[1:100]
+example=ds.iloc[56,9] #Getting the right column, i.e. the one with the review
 
 #Importing the model
-model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+model_name = "ProsusAI/finbert"
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+model=model.to(device)
+
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # With both the model and tokenizer initialized we are now able to get explanations on an example text.
@@ -21,7 +33,17 @@ from transformers_interpret import SequenceClassificationExplainer
 cls_explainer = SequenceClassificationExplainer(
     model,
     tokenizer)
-word_attributions = cls_explainer(ds)
+word_attributions = cls_explainer(example)
 cls_explainer.visualize("visualize.html")
 
-#Other explainability methods
+#Explaining using LIME:
+from lime.lime_text import LimeTextExplainer
+def predictor(texts):
+	outputs = model(**tokenizer(texts, return_tensors="pt", padding=True).to(device))
+	probas = F.softmax(outputs.logits).detach().numpy()
+	return probas
+
+class_names=['positive','negative','neutral']
+explainer = LimeTextExplainer(class_names=class_names)
+
+exp=explainer.explain_instance(example,predictor,num_features=20, num_samples=2000)
