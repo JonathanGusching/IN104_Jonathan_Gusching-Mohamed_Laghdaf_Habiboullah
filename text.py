edf9cc7efb2a6c8c617b7b1a9e73b3d50f4a2f69
JonathanGusching <jonathan.gusching@ensta-paris.fr>
2021-05-18 17:58:37 +0200

    Adding SHAP to text.py

--- text.py
+++ text.py
@@ -1,3 +1,5 @@
+
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from lime import lime_text
@@ -7,7 +9,7 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer
 #uses PyTorch
 
 device="cpu"
-torch.device(device)
+#torch.device(device)
 
 #We can't use our initial dataset (1GB), too heavy, so instead... :
 file_name="clothes.csv"
@@ -20,7 +22,7 @@ example=ds.iloc[15,4] #Getting the right column, i.e. the one with the review
 model_name = "ProsusAI/finbert"
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
-model=model.to(device)
+#model=model.to(device)
 
 tokenizer = AutoTokenizer.from_pretrained(model_name) #Note to myself: Tokenization= getting the words in the text, individually, and ignoring white spaces
 
@@ -36,7 +38,7 @@ word_attributions = cls_explainer(example)
 cls_explainer.visualize("visualize.html")
 
 #FIRST ATTEMPT: Explaining using LIME:
-#from lime.lime_text import LimeTextExplainer
+from lime.lime_text import LimeTextExplainer
 #def predictor(texts):
 #	outputs = model(**tokenizer(texts, return_tensors="pt", padding=True).to(device))
 #	probas = F.softmax(outputs.logits).detach().numpy() #memory problem here
@@ -50,9 +52,41 @@ cls_explainer.visualize("visualize.html")
 
 #https://shap.readthedocs.io/en/latest/example_notebooks/text_examples/sentiment_analysis/Positive%20vs.%20Negative%20Sentiment%20Classification.html
 
+#SECOND ATTEMPT
 #import eli5
 #from eli5.lime import TextExplainer
 
 #te = TextExplainer(model,random_state=42)
 #te.fit(example, model.eval())
 #te.show_prediction(target=example)#target_names=twenty_train.target_names)
+
+#THIRD ATTEMPT :
+#model = ClassificationModel('roberta', 'PATH_TO_FINE-TUNED_MODEL', use_cuda=True, args=args)
+
+# define softmax function
+#def softmax(x):
+#    e_x = np.exp(x - np.max(x))
+#    return e_x / e_x.sum(axis=0)
+# define prediction function
+#def predict_probs(texts):
+    #predictions = model.predict(texts)
+#	outputs = model(**tokenizer(texts, return_tensors="pt", padding=True))
+#	x = np.array(list(outputs)[1])
+#	return np.apply_along_axis(softmax, 1, x)
+#class_names=['positive','negative','neutral']
+#explainer = LimeTextExplainer(class_names=class_names)
+# explain instance with LIME
+#exp = explainer.explain_instance(example, predict_probs, num_features=6)
+import transformers
+import shap
+
+# load a transformers pipeline model
+model = transformers.pipeline('sentiment-analysis', return_all_scores=True)
+
+# explain the model on two sample inputs
+explainer = shap.Explainer(model) 
+shap_values = explainer(["What a great movie! ...if you have no taste."])
+
+# visualize the first prediction's explanation for the POSITIVE output class
+shap.plots.text(shap_values[0, :, "POSITIVE"])
+explainer.visualize("visualize.html")
